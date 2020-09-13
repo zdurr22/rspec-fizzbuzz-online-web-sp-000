b084e34b7cbeb655ee26d5f4786ac5789ba13f21
Zachary Durr <zdurr22@gmail.com>
2020-09-13 00:21:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 puts "Welcome to the Rental Car Agency fee Checker. What is your Age?"
 age = gets.strip.to_i
 if age < 21
-  "You are not able to rent a car. Have a good day!"
+  puts "You are not able to rent a car. Have a good day!"
 elsif age >= 25
-  "You are able to rent a car without fees. Have a good day!"
+  puts "You are able to rent a car without fees. Have a good day!"
 else
 
 end
