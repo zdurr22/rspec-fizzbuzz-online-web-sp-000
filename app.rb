edc9a9414a28f6014bb93d1928450b45cfb3f98f
Zachary Durr <zdurr22@gmail.com>
2020-09-13 00:28:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,17 +1,23 @@
 puts "Welcome to the Rental Car Agency fee Checker. What is your Age?"
 age = gets.strip.to_i
-if age < 21
-  puts "You are not able to rent a car. Have a good day!"
-elsif age >= 25
-  puts "You are able to rent a car without fees. Have a good day!"
-else
-  puts "How many days will you be renting for?"
-  rental_days = gets.strip.to_i
-  fee_per_day = 20
-  fee = rental_days * fee_per_day
-  puts "You will owe #{fee}"
-  
-  puts "What state are you from?"
-  state = gets.strip
+  if age < 21
+    puts "You are not able to rent a car. Have a good day!"
+  elsif age >= 25
+    puts "You are able to rent a car without fees. Have a good day!"
+  else
+    puts "How many days will you be renting for?"
+      rental_days = gets.strip.to_i
+      fee_per_day = 20
+      fee = rental_days * fee_per_day
+    puts "You will owe #{fee}"
+  end
+puts "What state are you from?"
+state = gets.strip
+  if state == "NY" || "New York" || "new york"
+    ny_state_fee = 20
+    puts "You owe $#{ny_state_fee}"
+  elsif state == "MI" || "Michigan" || "michigan"
+    mi_state_fee = 25
+    puts "You also owe $#{mi_state_fee}"
 
-end
+  end
